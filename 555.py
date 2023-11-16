f0a46ead31bb0fa8780915782e5eb188c70652f7
Kedabu <130380626+Kedabu@users.noreply.github.com>
2023-11-16 20:56:35 +0800

    Update 555.py

--- 555.py
+++ 555.py
@@ -13,3 +13,18 @@ pygame.display.set_caption("Plants vs Zombies")
 # 初始化游戏变量
  
 # 游戏主
+
+import pygame
+ 
+# 初始化pygame
+pygame.init()
+ 
+# 设置窗口大小和标题
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Plants vs Zombies")
+ 
+# 加载游戏资源
+# 初始化游戏变量
+ 
+# 游戏主
